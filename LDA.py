c650da16374d23a7d897e503e408c7ad1861c708
Lilly <lgr4641@rit.edu>
2024-11-20 11:47:34 -0500

    working around linearly separability, trying with RF to see how that looks. Possibly could be helpful to add to presentation to show how using LDA with RF improves accuracy

--- LDA.py
+++ LDA.py
@@ -3,21 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Loading the dataset
-data = pd.read_csv("data/diabetes_012_health_indicators_BRFSS2015.csv")
+data = pd.read_csv("data/diabetes_binary_5050split_health_indicators_BRFSS2015.csv")
 
 # Separate features and labels
 X = data.iloc[:, 1:].values # features are all the columns minus first column
 y = data.iloc[:, 0].values # features are the first column (diabetes type)
 
+# Spit data
+split_ratio = 0.8
+split_index = int(len(X) * split_ratio)
+indices = np.random.permutation(len(X))
+
+X_train, X_test = X[indices[:split_index]], X[indices[split_index:]]
+y_train, y_test = y[indices[:split_index]], y[indices[split_index:]]
+
 # Get class means, overall mean
-classes = np.unique(y)
-mean_vectors = {c: np.mean(X[y == c], axis=0) for c in classes}
-mean_overall = np.mean(X, axis=0)
+classes = np.unique(y_train)
+mean_vectors = {c: np.mean(X_train[y_train == c], axis=0) for c in classes}
+mean_overall = np.mean(X_train, axis=0)
+
 
 # normalizing the data: subtract the mean of each feature from each data point
-for row in X:
-    for col, _  in enumerate(row):
-        row[col] -= mean_overall[col]
+X_train_norm = X_train - mean_overall
+num_features = X_train.shape[1]
+wc_ms = np.zeros((num_features, num_features))
 
 # Within-class scatter matrix
 num_features = X.shape[1] # num features is equal to number of columns
@@ -29,18 +38,15 @@ wc_ms = np.zeros((num_features, num_features)) # within class scatter matrix ini
 bc_ms = np.zeros((num_features, num_features))
 
 for c in classes:
-    class_data = X[y==c] # getting data for each class
-    mean_c = mean_vectors[c].reshape(-1, 1) # getting mean from precomputed vector and reshaping for multiplication so its a col vector
-    
-    # Getting sum of (X-uc)(X-uc)^T
+    class_data = X_train[y_train == c]
+    mean_c = mean_vectors[c].reshape(-1, 1)
+
+    # Within-class scatter matrix contribution
     wc_ms += np.sum([(x.reshape(-1, 1) - mean_c) @ (x.reshape(-1, 1) - mean_c).T for x in class_data], axis=0)
 
-
-# Between-class scatter matrix
-for c in classes:
-    num_samples_c = np.sum(y == c) # getting how many sampels there are in c
+    # Between-class scatter matrix contribution
+    num_samples_c = len(class_data)
     mean_diff = (mean_vectors[c] - mean_overall).reshape(-1, 1)
-    # getting sum of num_samples_c * (uc - u)(uc - u)^T by comparing mean sample for class and overall mean sample
     bc_ms += num_samples_c * (mean_diff @ mean_diff.T)
 
 # getting eigenvals and eigenvectors of wc_ms^-1 * bc_ms
@@ -54,35 +60,38 @@ eigvals = eigenvalues[sorted_indices].real
 eigvecs = eigenvectors[:, sorted_indices].real
 
 # project the data onto top two eigen values --> 2d projection = good for visualization
-W = eigvecs[:, :2]
-X_lda = X @ W
+W2 = eigvecs[:, :2]
+W10 = eigvecs[:, :10]
+X_train_lda = X_train_norm @ W2
+X_test_lda = (X_test - mean_overall) @ W2
 
-blue_targets = X_lda[np.where(y == 0.0)]
-red_targets = X_lda[np.where(y == 1.0)]
-orange_targets = X_lda[np.where(y == 2.0)]
+blue_targets = X_train_lda[y_train == 0.0]
+red_targets = X_train_lda[y_train == 1.0]
+orange_targets = X_train_lda[y_train == 2.0]
 
-plt.scatter(
-        blue_targets[:, 0],
-        blue_targets[:, 1],
-        marker="o",
-        facecolors="none",
-        edgecolors="blue",
-    )
+plt.scatter(blue_targets[:, 0], blue_targets[:, 1], marker="o", edgecolors="blue", facecolors="none", label="Class 0")
+plt.scatter(red_targets[:, 0], red_targets[:, 1], marker="o", edgecolors="red", facecolors="none", label="Class 1")
+plt.scatter(orange_targets[:, 0], orange_targets[:, 1], marker="o", edgecolors="orange", facecolors="none", label="Class 2")
 
-plt.scatter(
-        red_targets[:, 0],
-        red_targets[:, 1],
-        marker="o",
-        facecolors="none",
-        edgecolors="red",
-    )
+plt.legend()
+plt.title("LDA Projection (2D)")
+plt.xlabel("LD1")
+plt.ylabel("LD2")
+plt.savefig("lda_projection_2D.png")
+plt.close()
 
-plt.scatter(
-        orange_targets[:, 0],
-        orange_targets[:, 1],
-        marker="o",
-        facecolors="none",
-        edgecolors="orange",
-    )
+#LDA classifier
+class_means_lda = {c: np.mean(X_train_lda[y_train == c], axis=0) for c in classes}
 
-plt.savefig("testing.png")
+
+def predict(x, class_means):
+    distances = {c: np.linalg.norm(x - mean) for c, mean in class_means.items()}
+    return min(distances, key=distances.get)
+
+# Predict for test data
+y_pred = np.array([predict(x, class_means_lda) for x in X_test_lda])
+accuracy = np.mean(y_pred == y_test)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+# try in 3d space, try in 1d space
+# make classifier and asses in different spaces
+
